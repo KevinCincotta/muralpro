d668d3d7b53d3225877eaa769dffeef016abf0f2
Jay Cincotta <jay.cincotta@gmail.com>
2025-03-27 11:18:07 -0700

    Finally have a clean baseline

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <h1>Welcome to the React App</h1>
+    </div>
+  );
 }
 
 export default App;
